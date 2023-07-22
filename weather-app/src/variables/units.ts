fe33001131c90bd42e7e4c1e8636ae5cce4eb5a3
Carlos Teixeira <s.carlosateixeira@gmail.com>
2023-07-22 22:37:59 +0100

    useAxios hook

---
+++ weather-app/src/variables/units.ts
@@ -0,0 +1,17 @@
+export enum metric {
+  temp = "&deg;C",
+  windSpeed= "m/s",
+  windGust= windSpeed
+}
+
+export enum imperial{
+  temp = "&deg;F",
+  windSpeed = "mph",
+  windGust = windSpeed
+}
+
+export enum standard{
+  temp = "&deg;K",
+  windSpeed = "m/s",
+  windGust = windSpeed
+}
